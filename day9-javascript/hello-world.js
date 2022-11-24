4be385b158a66e24c60519ceb4db802f756aeb39
PhongForger <giaphongminecraft@gmail.com>
2022-11-24 21:26:59 +0700

    hw

--- day9-javascript/hello-world.js
+++ day9-javascript/hello-world.js
@@ -1,2 +1,42 @@
-console.log("Hello World");
-console.log(abc);
+function greeting(name) {
+  console.log("Hello, " + name);
+  console.log("Welcome to my world");
+}
+
+greeting("Phong");
+greeting("Bob");
+
+alert("Hello");
+
+function add(a, b) {
+  console.log("Answer = " + (a + b));
+}
+
+add(29, 20);
+
+function sub(a, b) {
+  console.log("Answer = " + (a - b));
+}
+
+sub(29, 20);
+
+function mul(a, b) {
+  console.log("Answer = " + a * b);
+}
+
+mul(29, 20);
+
+function div(a, b) {
+  console.log("Answer = " + a / b);
+}
+
+div(29, 20);
+
+const currentYear = 2022;
+
+function calcAge(yearOfBirth) {
+  let age = currentYear - calcAge;
+  console.log("Your age is: " + age);
+}
+
+calcAge(2004);
---
+++ day9-javascript/onlab (1).js
@@ -0,0 +1,11 @@
+function add(a, b) {}
+
+function sub(a, b) {}
+
+function mul(a, b) {}
+
+function div(a, b) {}
+
+const currentYear = 2022;
+
+function calcAge(yearOfBirth) {}
